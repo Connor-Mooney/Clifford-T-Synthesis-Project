a828d8514f1c92eecaaf5ea84f82e1f7fa0d69d4
Connor-Mooney <37633758+Connor-Mooney@users.noreply.github.com>
2021-04-07 17:27:25 -0400

    Fleshing out operator class

--- operator.py
+++ operator.py
@@ -1,8 +1,11 @@
+import numpy as np
+
 class Operator:
   """This class represents a matrix in SO6(Z[1/sqrt(2)]), storing the name of the operator, i.e. a string of T operators, its actual matrix, its residue, 
   and its column-permutation-invariant class"""
-  def __init__(self, mat,name):
+  def __init__(self, tcount, mat,name):
     self.matrix = mat
+    self.tcount = tcount
     self.name = name
     self.residue = self.gen_res()
     self.permclass = self.column_perm_class()
@@ -10,10 +13,33 @@ class Operator:
   def gen_res(self):
     """Generates the residue of this operator"""
     return None
+  
+  def transpose(mat):
+    """returns the transpose of a matrix"""
+    vOut = []
+    for i in range(len(mat[0])):
+        vOut.append([mat[j][i] for j in range(len(mat))])
+    return vOut
+
+  
   def column_perm_class(self):
     """Generates the permutation class of this operator"""
-    return None
+    permclass = [[None, None, None, None, None, None], [None, None, None, None, None, None], \
+                 [None, None, None, None, None, None], [None, None, None, None, None, None],\
+                 [None, None, None, None, None, None], [None, None, None, None, None, None]]
+    
+    for i in range(self.matrix[0].length):
+      for j in range(self.matrix[0][0].length):
+        permclass[i][j] = (self.matrix[0][i][j], self.matrix[1][i][j])
+   
+   return [self.tcount, transpose(sorted(transpose(permclass)))]
+    
+ 
   def dot(self, op2):
     """multiplies this operator with another"""
+    tcountnew = self.tcount + op2.tcount
+    mat0 = np.matmul(self.matrix[0],op2.matrix[0]) + 2*np.matmul(self.matrix[1],op2.matrix[1])
+    mat1 = np.matmul(self.matrix[0],op2.matrix[1]) + np.matmul(self.matrix[1],op2.matrix[0])
+    newmat = [mat0, mat1]
     return None
     
